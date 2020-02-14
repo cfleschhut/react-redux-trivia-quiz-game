c591f8ad27d2b523ec63b57ee2b54f1773f81f99
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-02-14 12:53:18 +0100

    Fetch questions from API in stateful GameScreen component

--- src/components/GameScreen/index.js
+++ src/components/GameScreen/index.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { HeaderBar } from '../HeaderBar';
 import './styles.scss';
 
-export const GameScreen = () => {
-  return (
-    <div className="GameScreen">
-      <HeaderBar numOfQuestions={10} questionsAnswered={3} score={20} />
-    </div>
-  );
+const getApiUrl = () => {
+  const API_BASE_URL = 'https://opentdb.com/api.php';
+  const params = Object.entries({
+    amount: 3,
+    category: 9,
+    difficulty: 'easy',
+    type: 'multiple'
+  })
+    .map(([key, value], i) => `${i === 0 ? '?' : '&'}${key}=${value}`)
+    .join('');
+
+  return `${API_BASE_URL}${params}`;
 };
+
+class GameScreen extends Component {
+  state = {
+    questions: [],
+    questionsAnswered: [],
+    loading: false,
+    score: 0
+  };
+
+  componentDidMount() {
+    this.fetchQuestions();
+  }
+
+  fetchQuestions() {
+    this.setState(() => ({ loading: true }));
+
+    fetch(getApiUrl())
+      .then(res => res.json())
+      .then(data => {
+        this.setState(() => ({
+          questions: data.results,
+          loading: false
+        }));
+      })
+      .catch(e => console.log(e));
+  }
+
+  render() {
+    const { questions, questionsAnswered, score } = this.state;
+
+    return (
+      <div className="GameScreen">
+        <HeaderBar
+          numOfQuestions={questions.length}
+          questionsAnswered={questionsAnswered.length}
+          score={score}
+        />
+      </div>
+    );
+  }
+}
+
+export { GameScreen };
